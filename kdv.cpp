f769790f8fef77e0d3262c0e46e9281242465dbe
Michael Jørgensen <michael.finn.jorgensen@gmail.com>
2020-02-19 19:11:25 +0100

    WIP.

--- kdv.cpp
+++ kdv.cpp
@@ -13,17 +13,17 @@ int main()
 
    // The values below determine the region of interest.
    const double xmax = 40.0;
-   const double tmax = 0.1;
+   const double tmax = 10.0;
 
    // The values below control the accuracy of the calculations.
-   const double dx = 0.01;
-   const double dt = 0.000001;
+   const double dx = 0.1;
+   const double dt = 0.00001;
 
    // This function defines the initial condition.
    auto u0 = [&] (double x)
    {
       // Boundary values must be handled separately.
-      if (x<=0 || x>=xmax)
+      if (x<=0.0 || x>=xmax)
          return 0.0;
 
       // This transforms linearly from [0 .. xmax] to [-1 .. 1].
@@ -33,9 +33,11 @@ int main()
       const unsigned int STRETCH = 8;
 
       // This transforms nonlinearly from [-1 .. 1] to [-infinity .. infinity].
-      const double t = s/(1-pow(s,STRETCH));
+      const double t = s/(1-pow(s,STRETCH))*xmax*0.5;
 
-      double c = cosh(t / 2.0);
+      // Temporary variable.
+      const double c = cosh(t / 2.0);
+
       return -0.5/(c*c);
    };
 
@@ -63,7 +65,7 @@ int main()
    dv u(nmax+1);
    for (unsigned int n=0; n<=nmax; ++n)
    {
-      double x = n*dx;
+      const double x = n*dx;
       u[n] = u0(x);
    }
 
@@ -72,17 +74,13 @@ int main()
    for (unsigned int m=0; m<=mmax; ++m)
    {
       calc_ut(ut, u);
-      for (unsigned int n=0; n<=nmax; ++n)
-      {
-         u[n] += ut[n]*dt;
-      }
 
-      double t = m*dt;
-      if ((m%(nmax/1000)) == 0)
+      if ((m%(mmax/1000)) == 0)
       {
+         const double t = m*dt;
          for (unsigned int n=0; n<=nmax; ++n)
          {
-            double x = n*dx;
+            const double x = n*dx;
             std::cout <<    "t="  << std::setw(8) << std::setprecision(4) << t
                << " , x="  << std::setw(8) << std::setprecision(4) << x
                << " , u="  << std::setw(8) << u[n]
@@ -90,6 +88,11 @@ int main()
                << std::endl;
          }
          std::cout << std::endl;
+      }
+
+      for (unsigned int n=0; n<=nmax; ++n)
+      {
+         u[n] += ut[n]*dt;
       }
    }
 
